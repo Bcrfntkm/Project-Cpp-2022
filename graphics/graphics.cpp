d872d3422cb84968e83ebc1f0786a49a6c1110ec
default-boring-name <vasiakin.mm@phystech.edu>
2022-11-30 01:15:49 +0300

    Added clickable buttons and texture atlas. Modified cmake file to copy resources.

--- graphics/graphics.cpp
+++ graphics/graphics.cpp
@@ -1,10 +1,9 @@
 #include "graphics.h"
 #include <unistd.h>
+#include <iostream>
 
-Window::Window(SDL_Rect& frame):win_ptr(nullptr), frame(frame){}
-bool Window::init(){
+Window::Window(SDL_Rect& frame){
     win_ptr = SDL_CreateWindow("", frame.x, frame.y, frame.w, frame.h, SDL_WINDOW_SHOWN);
-    return win_ptr != nullptr;
 }
 Window::operator  bool() const {
     return win_ptr != nullptr;
@@ -16,11 +15,9 @@ Window::~Window(){
     SDL_DestroyWindow(win_ptr);
 }
 
-Renderer::Renderer(SDL_Rect& frame, Window& win): render_ptr(nullptr), frame(frame), win(win){}
-bool Renderer::init(){
+Renderer::Renderer(Window& win){
     render_ptr = SDL_CreateRenderer(win.get(), -1, SDL_RENDERER_SOFTWARE);
     SDL_SetRenderDrawBlendMode(render_ptr, SDL_BLENDMODE_BLEND);
-    return render_ptr != nullptr;
 }
 Renderer::operator bool() const{
     return render_ptr != nullptr;
@@ -39,12 +36,86 @@ Renderer::~Renderer(){
     SDL_DestroyRenderer(render_ptr);
 }
 
-App::App():frame({100,100,1000,600}), running(false), win(frame), 
-    renderer(frame, win){
+SafeTexture::SafeTexture(SDL_Surface *surf, Renderer &renderer):texture(nullptr){
+    if (surf != nullptr){
+        texture = SDL_CreateTextureFromSurface(renderer.get(), surf);
+        SDL_FreeSurface(surf);
+    }
+}
+SafeTexture::SafeTexture(SDL_Surface *surf, Renderer &renderer, SDL_Color key):texture(nullptr){
+    if (surf != nullptr){
+        SDL_SetColorKey(surf, SDL_TRUE, SDL_MapRGB(surf->format, key.r, key.g, key.b));
+        texture = SDL_CreateTextureFromSurface(renderer.get(), surf);
+        SDL_FreeSurface(surf);
+
+    }
+}
+SafeTexture::operator  bool() const {
+    return texture != nullptr;
+}
+SDL_Texture* SafeTexture::get(){
+    return texture;
+}
+
+SafeTexture::~SafeTexture(){
+    if(texture != nullptr)
+        SDL_DestroyTexture(texture);
+}
+
+
+Button::Button(SDL_Rect off_texture, SDL_Rect hover_texture, SDL_Rect on_texture, SDL_Rect rect, App& app, void (App::*trigger)()): 
+    off_texture(off_texture), hover_texture(hover_texture), on_texture(on_texture), rect(rect), app(app), trigger(trigger), state(ButtonState::OFF), click_timer(0){}
+void Button::press(){
+                    if(click_timer == 0){
+                        state = ButtonState::ON; 
+                    }
+}
+void Button::hover(){if(click_timer == 0) state = ButtonState::HOVER;}
+void Button::off(){if(click_timer == 0) state = ButtonState::OFF;}
+void Button::release(){if(click_timer == 0) {click_timer = 3; (app.*trigger)();}}
+void Button::draw(Renderer &renderer, SafeTexture &texture_atlas){
+    if (state == OFF){
+        SDL_RenderCopy(renderer.get(), texture_atlas.get(), &off_texture, &rect);
+    }else if (state == HOVER){
+        SDL_RenderCopy(renderer.get(), texture_atlas.get(), &hover_texture, &rect);
+    }else if (state == ON){
+        SDL_RenderCopy(renderer.get(), texture_atlas.get(), &on_texture, &rect);
+    }
+}
+void Button::update(){
+    if (click_timer > 0){
+        click_timer --;
+        if (click_timer == 0)
+            hover();
+    }
+}
+SDL_Rect const & Button::get_rect(){return rect;}
+
+
+App::App():frame({100,100,1000,600}), running(false), mode(NORMAL) 
+    {
     if(SDL_Init(SDL_INIT_VIDEO) == 0){
-        if (win.init()){
-            if (renderer.init()){
-                running = true;
+        win.reset(new Window(frame));
+        if (win){
+            renderer.reset(new Renderer(*win.get()));
+            if (renderer){
+                texture_atlas.reset(new SafeTexture(SDL_LoadBMP("resources/texture_atlas.bmp"), *renderer,SDL_Color({255, 0, 255, 255})));
+                if (texture_atlas){
+
+                    buttons[0].reset(new Button(
+                            SDL_Rect({0,0,100,100}), SDL_Rect({100,0,100,100}), SDL_Rect({200,0,100,100}), 
+                            SDL_Rect({10,10,100,100}), *this, &App::home));
+                    buttons[1].reset(new Button(
+                            SDL_Rect({0,100,100,100}), SDL_Rect({100,100,100,100}), SDL_Rect({200,100,100,100}), 
+                            SDL_Rect({10,115,100,100}), *this, &App::refresh));
+                    buttons[2].reset(new Button(
+                            SDL_Rect({0,200,100,100}), SDL_Rect({100,200,100,100}), SDL_Rect({200,200,100,100}), 
+                            SDL_Rect({10,220,100,100}), *this, &App::add_mode));
+                    buttons[3].reset(new Button(
+                            SDL_Rect({0,300,100,100}), SDL_Rect({100,300,100,100}), SDL_Rect({200,300,100,100}), 
+                            SDL_Rect({10,325,100,100}), *this, &App::move_mode));
+                    running = true;
+                }
             }
         }
     }
@@ -52,19 +123,52 @@ App::App():frame({100,100,1000,600}), running(false), win(frame),
 void App::proccess_events(){
     SDL_Event event; 
     while(SDL_PollEvent(&event)){
-        if (event.type == SDL_QUIT)
+        if (event.type == SDL_QUIT){
             running = false;
+        }else if(event.type == SDL_MOUSEBUTTONDOWN){
+            SDL_Point mouse_pos({event.button.x, event.button.y});
+            for (auto it = buttons.begin(); it != buttons.end(); ++it){
+                if (SDL_PointInRect(&mouse_pos, &((*it)->get_rect())))
+                (*it)->press();
+            }
+
+        }else if(event.type == SDL_MOUSEBUTTONUP){
+            SDL_Point mouse_pos({event.button.x, event.button.y});
+
+            for (auto it = buttons.begin(); it != buttons.end(); ++it){
+                if (SDL_PointInRect(&mouse_pos, &((*it)->get_rect())))
+                (*it)->release();
+            }
+
+        }else if(event.type == SDL_MOUSEMOTION){
+            SDL_Point mouse_pos({event.motion.x, event.motion.y});
+            for (auto it = buttons.begin(); it != buttons.end(); ++it){
+                if (SDL_PointInRect(&mouse_pos, &((*it)->get_rect())))
+                    (*it)->hover();
+                else{
+                    (*it)->off();
+                }
+            }
+        }
     }
 }
 void App::loop(){
+    for (auto it = buttons.begin(); it != buttons.end(); ++it){
+        (*it)->update();
+    }
+
 }
 void App::render(){
     for (unsigned i = 0; i < frame.h; ++i){
         for (unsigned j = 0; j < frame.w; ++j){
-            renderer.draw_point(SDL_Color({0,0,0,255}), SDL_Point({j, i}));
+            renderer->draw_point(SDL_Color({0,0,0,255}), SDL_Point({j, i}));
         }
     }
-    renderer.update();
+    for (auto it = buttons.begin(); it != buttons.end(); ++it){
+        (*it)->draw(*renderer, *texture_atlas);
+    }
+    
+    renderer->update();
 }
 void App::run(){
     while(running){
@@ -74,6 +178,19 @@ void App::run(){
         sleep(0.03);
     }
 }
+void App::add_mode(){
+    mode = Mode::ADD;
+}
+void App::refresh(){
+    mode = Mode::NORMAL;
+}
+void App::home(){
+    mode = Mode::NORMAL;
+}
+void App::move_mode(){
+    mode = Mode::MOVE;
+}
+
 App::~App(){
     SDL_Quit();
 } 
--- graphics/graphics.h
+++ graphics/graphics.h
@@ -1,42 +1,84 @@
 #ifndef graphics
 #define graphics
 #include "SDL.h"
+#include <memory>
+#include <array>
+#include <list>
 
+class App;
 class Window{
     SDL_Window *win_ptr;
-    SDL_Rect const & frame;
 public:
     Window(SDL_Rect& frame);
-    bool init();
     operator  bool() const;
     SDL_Window* get();
     ~Window();
 };
 class Renderer{
     SDL_Renderer *render_ptr;
-    Window &win;
-    SDL_Rect const &  frame;
 public:
-    Renderer(SDL_Rect& frame, Window& win);
-    bool init();
+    Renderer(Window& win);
     operator  bool() const;
     void draw_point(SDL_Color color, SDL_Point p);
     void update();
     SDL_Renderer* get();
     ~Renderer();
 };
+
+class SafeTexture{
+    SDL_Texture * texture;
+public:
+    SafeTexture(SDL_Surface *surf, Renderer& renderer);
+    SafeTexture(SDL_Surface *surf, Renderer& renderer, SDL_Color key);
+    operator  bool() const;
+    SDL_Texture * get();
+    ~SafeTexture();
+};
+
+class Button{
+public:
+    enum ButtonState {OFF, HOVER, ON};
+private:
+    ButtonState state;
+    SDL_Rect off_texture;
+    SDL_Rect hover_texture;
+    SDL_Rect on_texture;
+    App & app;
+    void (App::*trigger)();
+    SDL_Rect rect;
+    int click_timer;
+public:
+    Button(SDL_Rect off_texture, SDL_Rect hover_texture, SDL_Rect on_texture, SDL_Rect rect, App & app, void (App::*trigger)());
+    void press();
+    void release();
+    void hover();
+    void off();
+    void draw(Renderer &renderer, SafeTexture &texture_atlas);
+    void update();
+    SDL_Rect const & get_rect();
+};
+
 class App{
+public:
+    enum Mode {NORMAL, ADD, MOVE};
 private:
     SDL_Rect frame;
+    Mode mode;
     bool running;
-    Window win;
-    Renderer renderer;
+    std::unique_ptr<Window> win;
+    std::unique_ptr<Renderer> renderer;
+    std::unique_ptr<SafeTexture> texture_atlas;
+    std::array<std::unique_ptr<Button>, 4> buttons; 
 public:
     App();
     void loop();
     void proccess_events();
     void render();
     void run();
+    void add_mode();
+    void move_mode();
+    void refresh();
+    void home();
     ~App();
 };
 #endif
